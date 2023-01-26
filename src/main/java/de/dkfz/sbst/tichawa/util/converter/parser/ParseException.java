dcaf5cc3b856a8808ebb2739e73ac2b12aac3bb7
Nightara <f.tichawa@dkfz.de>
2023-01-26 13:44:21 +0100

    Added data field to ParseException

--- src/main/java/de/dkfz/sbst/tichawa/util/converter/parser/ParseException.java
+++ src/main/java/de/dkfz/sbst/tichawa/util/converter/parser/ParseException.java
@@ -1,9 +1,18 @@
 package de.dkfz.sbst.tichawa.util.converter.parser;
 
+import lombok.*;
+
+import java.io.*;
+
+@Value
+@EqualsAndHashCode(callSuper=true)
 public class ParseException extends IllegalArgumentException
 {
-  public ParseException(String s)
+  Serializable data;
+
+  public ParseException(String s, Serializable data)
   {
     super(s);
+    this.data = data;
   }
 }
