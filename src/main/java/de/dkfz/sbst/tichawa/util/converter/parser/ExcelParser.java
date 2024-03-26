61934be689269a364b9703263186984ed3e2ed8a
Nightara <f.tichawa@dkfz.de>
2024-03-26 14:55:12 +0100

    Added default locale to Excel formatter timestamp parsing

--- src/main/java/de/dkfz/sbst/tichawa/util/converter/parser/ExcelParser.java
+++ src/main/java/de/dkfz/sbst/tichawa/util/converter/parser/ExcelParser.java
@@ -85,11 +85,13 @@ public class ExcelParser extends ReactiveParser<Row, Row>
       else if(value.rule().getOutType().equals(Configuration.DataType.INSTANT)
           || value.rule().getOutType().equals(Configuration.DataType.LOCAL_DATE))
       {
-        cell.setCellValue(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format((TemporalAccessor) value.data()));
+        cell.setCellValue(DateTimeFormatter.ISO_LOCAL_DATE_TIME.withLocale(Locale.ENGLISH)
+            .format((TemporalAccessor) value.data()));
       }
       else if(value.rule().getOutType().equals(Configuration.DataType.DURATION))
       {
-        cell.setCellValue(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDate.EPOCH.plus((Duration) value.data())));
+        cell.setCellValue(DateTimeFormatter.ISO_LOCAL_DATE_TIME.withLocale(Locale.ENGLISH)
+            .format(LocalDate.EPOCH.plus((Duration) value.data())));
       }
     }
 
